8e6c32eb820d7b76fa70854f62a57dab127e8121
Amitkumar-R <amitbr2128@gmail.com>
2022-03-24 19:48:30 +0530

    initial commit

---
+++ e-medicare-app-backnd/src/test/java/com/medicare/EMedicareAppBackndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EMedicareAppBackndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
